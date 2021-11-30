42ad83949712a02e41a52e50e74b0eb598dadcf2
brymopo <bmoreno.dl@gmail.com>
2021-11-30 18:35:09 -0500

    add comments to post type

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,5 +5,6 @@ module Types
     field :id, ID, null: false
     field :body, String, null: false
     field :user, Types::UserType, null: false
+    field :comments, [Types::CommentType], null: true
   end
 end
