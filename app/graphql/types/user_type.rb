6b15b81b3799bfbc4767b81681d9d8c13d14e0b5
brymopo <bmoreno.dl@gmail.com>
2021-11-30 18:26:47 -0500

    add User id to fields in user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < GraphQL::Schema::Object
     description "One User"
 
+    field :id, ID, null: false
     field :first_name, String, null: false
     field :last_name, String, null: false
     field :street, String, null: true
